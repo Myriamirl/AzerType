04f8c73b20f44fe8120eeb16b5ac700e5b3f5b9f
morgiane <taleb.morgiane@gmail.com>
2025-03-01 12:55:20 +0100

    avancer projet par dossier

---
+++ scripts/config.js
@@ -0,0 +1,3 @@
+// Liste des mots et phrases
+const list_Mots = ["Cachalot", "Pétunia", "Serviette"];
+const list_Phrases = ["Pas de panique", "La vie, l'univers et le reste", "Merci pour le poisson"];
